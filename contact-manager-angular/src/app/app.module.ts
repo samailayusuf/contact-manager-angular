0b068d956b108e9ec24f661d1cc1ded5286b3ed7
Yusuf Samaila <yusuf7samaila@gmail.com>
2022-03-03 22:35:53 +0100

    route components added

--- contact-manager-angular/src/app/app.module.ts
+++ contact-manager-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { AboutComponent } from './about/about.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddComponent,
+    AboutComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
