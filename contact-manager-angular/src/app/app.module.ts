e4dca41398526c0d236ebe42a3b52d63ccc7fc5b
Yusuf Samaila <yusuf7samaila@gmail.com>
2022-03-04 09:27:16 +0100

    add

--- contact-manager-angular/src/app/app.module.ts
+++ contact-manager-angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddComponent } from './add/add.component';
 import { AboutComponent } from './about/about.component';
 import { EditComponent } from './edit/edit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,22 @@ import { EditComponent } from './edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { 
+        path: '',
+        component: HomeComponent
+      },
+      { 
+        path: 'add',
+        component: AddComponent
+      },
+    { 
+      path: 'about',
+      component: AboutComponent
+   }
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
